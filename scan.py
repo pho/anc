46c5e95025601eaea8b353ccc482f12b08c9adb4
pho <phofin@gmail.com>
2011-06-10 22:12:37 +0200

    Unified the indentation style. Removed xte dependency, now using python-xlib

--- scan.py
+++ scan.py
@@ -7,40 +7,73 @@
 # 0 = Pushed
 
 #Requeriments:
-# xte
+# X
+# python-xlib
 
+#Usage example:
+# python scan.py /dev/ttyUSB0
 
 #Config
 # A B SELECT START UP DOWN LEFT RIGHT
-but = ["a", "b", "Shift_R", "Return", "Up", "Down", "Left", "Right"]
-delay = 0.0 #Scan might be too fast
+
+#Why are the default config set to Control/Alt/Super?
+#Because of the autorepeat delay, it's dificult to play anything
+#it the key do some delay. The keypresses are sent to the X, so is an
+#system config issue. If you know how to do this better, contact me ;D
+
+but = ["Alt_L", "Control_R", "f", "g", "Super_L", "Super_R", "Shift_R", "Shift_L"]
+
+delay = 0.1 #Scan might be too fast
 
 import serial, os, sys, signal
+from Xlib import X, XK  
+from Xlib.display import Display  
+from Xlib.ext import xtest  
 from time import sleep
+
+if len(sys.argv) < 2:
+  print "(1) Argument needed"
+  print "Usage: scan.py /dev/ttyUSB0"
+  sys.exit(-1)
 
 #catch ctrl+c
 def signal_handler(signal, frame):
-		print "exiting..."
-		sys.exit(0)
-		
+    print "exiting..."
+    d.flush()
+    sys.exit(0)
 signal.signal(signal.SIGINT, signal_handler)
 
-ser = serial.Serial("/dev/ttyUSB1", 9600)
+d = Display()  
+
+#Transform the keys strings to keycodes
+for i in range(0, len(but)):  
+    but[i] = d.keysym_to_keycode(XK.string_to_keysym(but[i]))  
+
+ser = serial.Serial(sys.argv[1], 9600)
 
 while 1:
-	ser.write('a');
-	
-	actual = ser.read()
-	while (actual == -1):
-		actual = ser.read()
-	actual += ser.readline()
+  ser.write('a');
+  
+  try:
+    actual = ser.read()
+    while (actual == -1):
+      actual = ser.read()
+    actual += ser.readline()
+  except:
+    print "Something is wrong. Restarting..."
+    ser.close()
+    ser = serial.Serial(sys.argv[1], 9600)
+  if len(actual) != 9:
+    continue #Bad
 
-	if len(actual) != 9:
-		continue #Bad
-
-	for i in range(0, 8):
-		if list(actual)[i] == '0':
-			os.system("xte \"keydown " + but[i] + "\"")
-		else:
-			os.system("xte \"keyup " + but[i] + "\"")
-	sleep(delay)
+  for i in range(0, 8):
+    if list(actual)[i] == '0':
+      xtest.fake_input(d, X.KeyPress, but[i])  
+      d.sync()
+      d.flush()
+    else:
+      xtest.fake_input(d, X.KeyRelease, but[i])  
+      d.sync() 
+      d.flush()
+      
+  sleep(delay)
